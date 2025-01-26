e303c550ffcdeed4cf9d81c277ad841030b7fbf5
Ihar Yazerski <ihar.yazerski@gmail.com>
2025-01-26 01:35:16 +0100

    add proper handling for some popular errors

--- pit38/main.py
+++ pit38/main.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from pit38.brokers import freedom24
 from pit38.brokers.base import SupportedBroker
+from pit38.exceptions import Pit38Exception
 from pit38.exchange.rates import fill_exchange_rates
 from pit38.io.xlsx import read_trades_from_xlsx, write_closed_positions_to_xlsx
 from pit38.positions.profit_calculator import calculate_profit
@@ -15,17 +16,23 @@ def process_annual_report(broker: SupportedBroker, tax_report_file: Path) -> Non
         case SupportedBroker.freedom24:
             adapter = freedom24.Freedom24Adapter()
         case _:
-            raise ValueError(f"Broker '{broker}' not supported yet")
+            raise Pit38Exception(f"Broker '{broker}' not supported yet")
 
     # read annual tax report (trades) from XLSX
     tax_report_file = tax_report_file.resolve()
+    if not tax_report_file.exists():
+        raise Pit38Exception(f"'{tax_report_file}' does not exist")
     raw_data = read_trades_from_xlsx(tax_report_file)
 
     # convert raw data into a unified structure
     trades = adapter.parse_trades(raw_data)
+    if not trades:
+        raise Pit38Exception(f"'{tax_report_file}' does not contain any trades")
 
     # apply FIFO logic
     closed_positions = match_trades_fifo(trades)
+    if not closed_positions:
+        raise Pit38Exception(f"'{tax_report_file}' does not contain any closed positions")
 
     # fill exchange rates
     fill_exchange_rates(closed_positions)
